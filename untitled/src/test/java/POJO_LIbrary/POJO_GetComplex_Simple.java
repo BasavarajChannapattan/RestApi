62cba9142e8b9e3624a619f7a7d638f89ec66872
BasavarajGC <107638236+BasavarajChannapattan@users.noreply.github.com>
2025-01-14 19:40:06 +0530

    'First Commit'
    Added Rest assured framework

---
+++ untitled/src/test/java/POJO_LIbrary/POJO_GetComplex_Simple.java
@@ -0,0 +1,21 @@
+package POJO_LIbrary;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class POJO_GetComplex_Simple {
+
+    public static void main(String[] args) {
+
+
+        Response res = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("http://localhost:3000/friends");
+
+        System.out.println("response status code "+ res.getStatusCode());
+        System.out.println("response body "+ res.getBody().asString());
+    }
+}
