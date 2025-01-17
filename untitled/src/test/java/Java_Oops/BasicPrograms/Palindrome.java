0b6a456ac4bbef777251d08392b6ea39a25b39c3
BasavarajGC <107638236+BasavarajChannapattan@users.noreply.github.com>
2025-01-17 13:08:09 +0530

    Added java basic prograams

--- untitled/src/test/java/Java_Oops/BasicPrograms/Palindrome.java
+++ untitled/src/test/java/Java_Oops/BasicPrograms/Palindrome.java
@@ -5,45 +5,53 @@ public class Palindrome {
     public static void main(String[] args) {
 
 
-//        int r, sum=0, temp;
-//
-//        int n= 121;
-//        temp = n;
-//
-//        while(n>0)
-//        {
-//            r= n%10;
-//            sum= (sum*10) +r;
-//            n= n/10;
-//        }
-//
-//        if(temp == sum)
-//        {
-//            System.out.println( " is a palindrome.");
-//        }
-//        else
-//        {
-//            System.out.println( " is not a palindrome.");
-//        }
+        int r, sum=0, temp;
 
-//        //approach =02
-//        String str ="12321", reversedString="";
-//
-//        int strLength=str.length();
-//
-//        for(int i= (strLength-1); i>=0; i--)
-//        {
-//            reversedString = reversedString+str.charAt(i);
-//        }
-//
-//        if(str.toLowerCase().equals(reversedString.toLowerCase()))
-//        {
-//            System.out.println(str + " is a palindrome.");
-//        }
-//        else
-//        {
-//            System.out.println(str + " is not a palindrome.");
-//        }
+        int n= 121;
+        temp = n;
+
+        while(n>0)
+        {
+            r= n%10;
+            sum= (sum*10) +r;
+            n= n/10;
+        }
+
+        if(temp == sum)
+        {
+            System.out.println( " is a palindrome.");
+        }
+        else
+        {
+            System.out.println( " is not a palindrome.");
+        }
+
+        //approach =02
+        String str ="12321", reversedString="";
+
+        int strLength=str.length();
+
+        for(int i= (strLength-1); i>=0; i--)
+        {
+            reversedString = reversedString+str.charAt(i);
+        }
+
+        if(str.toLowerCase().equals(reversedString.toLowerCase()))
+        {
+            System.out.println(str + " is a palindrome.");
+        }
+        else
+        {
+            System.out.println(str + " is not a palindrome.");
+        }
+
+
+        String str1 = "madam";  // Example string
+        if (str1.equals(new StringBuilder(str).reverse().toString())) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
 
 
     }
